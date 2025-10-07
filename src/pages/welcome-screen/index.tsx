d421cff5047498fb41ac0faf6b2411228ce92c04
Gabriel Lira <gabrieldeio40@gmail.com>
2025-10-07 15:03:44 -0300

    refactor: adjust screen due modifications

--- src/pages/welcome-screen/index.tsx
+++ src/pages/welcome-screen/index.tsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
-import CustomButton from '../../components/custom-button';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../../types/navigations';
-import { styles } from './styles';
+import React from 'react'
+import { View, Text, StatusBar } from 'react-native'
+import CustomButton from '../../components/custom-button'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../../types/navigations'
+import { styles } from './styles'
 
+type Props = NativeStackScreenProps<RootStackParamList, 'WelcomeScreen'>
 
-type Props = NativeStackScreenProps<RootStackParamList, 'WelcomeScreen'>;
-
-const WelcomeScreen = ({navigation} : Props) => {
+const WelcomeScreen = ({ navigation }: Props) => {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="dark-content" backgroundColor="#F5F5F5" />
+      <StatusBar barStyle='dark-content' backgroundColor='#F5F5F5' />
       <Text style={styles.headerTitle}>MeuEstagio.app</Text>
       <View style={styles.content}>
         <Text style={styles.title}>Bem-vindo(a)!</Text>
         <Text style={styles.subtitle}>Escolha uma opção para continuar.</Text>
         <CustomButton
-          title="Login"
-          onPress={() => navigation.navigate('LoginNew')}
+          title='Login'
+          onPress={() => navigation.navigate('Login')}
           style={styles.loginButton}
           textStyle={styles.loginButtonText}
         />
         <CustomButton
-          title="Cadastrar"
-          onPress={() => navigation.navigate('RegisterSupervisor')}
+          title='Cadastrar'
+          onPress={() => navigation.navigate('RegisterUser')}
           style={styles.registerButton}
           textStyle={styles.registerButtonText}
         />
       </View>
     </View>
-  );
-};
+  )
+}
 
-
-
-export default WelcomeScreen;
+export default WelcomeScreen
